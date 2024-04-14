7d23ca89b1e72743393acb8f2bc84db23251be65
Thu Vo <127747452+VNMThu@users.noreply.github.com>
2024-04-14 16:46:14 +0700

    - Add weapon rotation and lock on

--- RubyAdventure/Assets/Scripts/Weapon/BaseWeapon.cs
+++ RubyAdventure/Assets/Scripts/Weapon/BaseWeapon.cs
@@ -1,46 +1,111 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class BaseWeapon : MonoBehaviour
 {
-    [SerializeField] protected float rangeToDetectEnemies;
     [SerializeField] protected float fireRate;
-    [SerializeField] protected float damageEachShot;
-    [SerializeField] protected string detectionTag;
     [SerializeField] protected GameObject bullet;
+    protected SpriteRenderer _spriteRenderer;
     protected Transform currentTarget;
-    protected virtual void FireBullet()
+    protected bool _isSpriteFlipLeft;
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    protected virtual IEnumerator C_FireBullet()
     {
         //Fire off this bullet - Passing in target here
-        Instantiate(bullet);
-    }
-
-    protected virtual IEnumerator C_FiringShots()
-    {
-        //Call fire
-        FireBullet();
-        yield return new WaitForSeconds(1f / fireRate);
-    }
-    protected virtual void OnDetectEnemies(){}
-
-    protected void OnTriggerEnter2D(Collider2D other)
-    {
-        if (!other.CompareTag(detectionTag)) return;
         
-        //Fire
-        currentTarget = other.transform;
-        StartCoroutine(C_FiringShots());
+        // Instantiate(bullet);
+        yield return new WaitForSeconds(1 / fireRate);
+    }
+    
+    
+
+    protected virtual IEnumerator C_RotateWeapon()
+    {
+        //Rotate to target
+        while (!CheckHitEnemy())
+        {
+            float angle = Mathf.Atan2(currentTarget.position.y - transform.position.y,
+                currentTarget.position.x - transform.position.x) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle));
+            var rotation = transform.rotation;
+            
+            rotation = Quaternion.RotateTowards(rotation, targetRotation,
+                360f);
+            
+            transform.rotation = rotation;
+            
+            float currentAngleOfGun = rotation.eulerAngles.z;
+            
+            //Flip sprite correct to rotation
+            
+            switch (currentAngleOfGun)
+            {
+                case > 90 and < 270 when !_isSpriteFlipLeft:
+                    _spriteRenderer.flipY = !_spriteRenderer.flipY;
+                    _isSpriteFlipLeft = true;
+                    break;
+                case < 90 or > 270 when _isSpriteFlipLeft:
+                    _spriteRenderer.flipY = !_spriteRenderer.flipY;
+                    _isSpriteFlipLeft = false;
+                    break;
+            }
+
+            yield return null;
+
+        }
+        
+        //If rotate hit enemy -> Fire bullet
+        StartCoroutine(C_FireBullet()) ;
+        
+    }
+
+    protected virtual bool CheckHitEnemy()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, 1.5f, LayerMask.GetMask("Character"));
+
+        //Check if hit anything at all
+        if (hit.collider == null) return false; 
+        
+        //Check if hit enemy
+        if(hit.collider.CompareTag("Enemy")) return true;
+
+        //Hit something else
+        return false;
+    }
+
+    protected void OnTriggerStay2D(Collider2D other)
+    {
+        if (!other.CompareTag("Enemy")) return;
+
+        //Find the enemy that closer
+        if (currentTarget != null)
+        {
+            if (Vector3.Distance(other.transform.position, transform.position) <
+                Vector3.Distance(currentTarget.position, transform.position))
+            {
+                currentTarget = other.transform;
+            }
+        }
+        else
+        {
+            currentTarget = other.transform;
+        }
+
+
+        //Start rotate toward that enemy
+        StartCoroutine(C_RotateWeapon());
     }
 
     protected void OnTriggerExit2D(Collider2D other)
     {
-        if (!other.CompareTag(detectionTag)) return;
+        if (!other.CompareTag("Enemy")) return;
         
         //Fire
-        StopCoroutine(C_FiringShots());
-        currentTarget = null;
+        StopCoroutine(C_RotateWeapon());
     }
 }
