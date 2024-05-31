0788db182861698b0a368dd241a8ff8972955f71
Thu Vo <20110190@student.hcmute.edu.vn>
2024-05-31 17:41:05 +0700

    - Set Up display UI (IP)

--- RubyAdventure/Assets/Scripts/UI/ButtonUpgradeWeapon.cs
+++ RubyAdventure/Assets/Scripts/UI/ButtonUpgradeWeapon.cs
@@ -1,18 +1,87 @@
-using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonUpgradeWeapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TextMeshProUGUI weaponName;
+    [SerializeField] private TextMeshProUGUI levelOrUnlock;
+    [SerializeField] private Image weaponIcon;
+    [Header("Level")]
+    [SerializeField] private TextMeshProUGUI previousLevel;
+    [SerializeField] private Image levelArrowIcon;
+    [SerializeField] private TextMeshProUGUI AfterLevel;
+    [Header("Speed")]
+    [SerializeField] private TextMeshProUGUI previousSpeed;
+    [SerializeField] private Image speedArrowIcon;
+    [SerializeField] private TextMeshProUGUI AfterSpeed;
+    [Header("Power")]
+    [SerializeField] private TextMeshProUGUI previousPower;
+    [SerializeField] private Image powerArrowIcon;
+    [SerializeField] private TextMeshProUGUI AfterPower;
+
+    [Header("Sprites For Weapon")] [SerializeField]
+    private Sprite pistolSprite;
+    [SerializeField]
+    private Sprite rifleSprite;
+    [SerializeField]
+    private Sprite spearSprite;
+    public void InitUI(KeyValuePair<WeaponAttributeSuit,int> pairValue)
     {
+        //Name
+        weaponName.text = pairValue.Key.Type.ToString();
+        
+        if (pairValue.Value == 0)
+        {
+            //Unlock State
+            levelOrUnlock.text = "New!";
+            
+            //Set text
+            WeaponAttribute attribute = pairValue.Key.FindAttributeWithLevel(pairValue.Value);
+            AfterLevel.text = attribute.Level.ToString();
+            AfterSpeed.text = attribute.FireRate.ToString("0.0");
+            AfterPower.text = attribute.DamagePerAttack.ToString();
+            
+            //Turn off previous and icon
+            previousLevel.gameObject.SetActive(false);
+            previousSpeed.gameObject.SetActive(false);
+            previousPower.gameObject.SetActive(false);
+            levelArrowIcon.gameObject.SetActive(false);
+            speedArrowIcon.gameObject.SetActive(false);
+            powerArrowIcon.gameObject.SetActive(false);
+        }
+        else
+        {
+            //Unlock State
+            levelOrUnlock.text = "+1 Level";
+            
+            //Turn on UI
+            previousLevel.gameObject.SetActive(true);
+            previousSpeed.gameObject.SetActive(true);
+            previousPower.gameObject.SetActive(true);
+            levelArrowIcon.gameObject.SetActive(true);
+            speedArrowIcon.gameObject.SetActive(true);
+            powerArrowIcon.gameObject.SetActive(true);
+            
+            WeaponAttribute attributeBefore = pairValue.Key.FindAttributeWithLevel(pairValue.Value);
+
+            //Set text for before level
+            previousLevel.text = attributeBefore.Level.ToString();
+            previousSpeed.text = attributeBefore.FireRate.ToString("0.0");
+            previousPower.text = attributeBefore.DamagePerAttack.ToString();
+            
+            //Set text for after level
+            WeaponAttribute attributeAfter = pairValue.Key.FindAttributeWithLevel(pairValue.Value+1);
+            AfterLevel.text = attributeAfter.Level.ToString();
+            AfterSpeed.text = attributeAfter.FireRate.ToString("0.0");
+            AfterPower.text = attributeAfter.DamagePerAttack.ToString();
+            
+
+            
+        }
+        
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- RubyAdventure/Assets/Scripts/UI/LevelUpPanel.cs
+++ RubyAdventure/Assets/Scripts/UI/LevelUpPanel.cs
@@ -1,20 +1,72 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class LevelUpPanel : UIPanel
 {
     private Action<object> _onLevelUpRef;
+    [SerializeField] private ButtonUpgradeWeapon[] buttonUpgradeWeapons;
     private void OnEnable()
     {
         //Open when level up
         _onLevelUpRef = _ => OnOpen();
         EventDispatcher.Instance.RegisterListener(EventID.OnLevelUp,_onLevelUpRef);
     }
+    
+    
 
     public override void OnOpen()
     {
         Time.timeScale = 0f;
         base.OnOpen();
+    }
+
+    private void SetUpUpgradeSlot()
+    {
+        //Get all 
+        Dictionary<WeaponAttributeSuit, int> allData = GameManager.Instance.WeaponAttributeManagers.GetAllWeaponDataInfo();
+
+        foreach (var VARIABLE in allData.ToList())
+        {
+            //Filter it
+            // Already Reach max level
+            if (VARIABLE.Value > VARIABLE.Key.WeaponAttributePerLevel.Length)
+            {
+                //Remove
+                allData.Remove(VARIABLE.Key);
+            }
+        }
+        
+        //Find 3 random element
+        List<int> randomIndex = new List<int>();
+        int previousIndex = -1;
+        while (randomIndex.Count < buttonUpgradeWeapons.Length || randomIndex.Count < allData.Count)
+        {
+            int randomInt = Random.Range(0, allData.Count);
+            if (randomInt == previousIndex) continue;
+            randomIndex.Add(randomInt);
+            previousIndex = randomInt;
+        }
+        
+        //Turn it into UI
+
+        int differentBetweenUIAndData = allData.Count - buttonUpgradeWeapons.Length;
+
+        for (int i = 0; i < differentBetweenUIAndData; i++)
+        {
+            buttonUpgradeWeapons.Last().gameObject.SetActive(false);
+        }
+
+        int indexOfRandom = 0;
+        foreach (var buttonUpgrade in buttonUpgradeWeapons)
+        {
+            if (!buttonUpgrade.gameObject.activeInHierarchy) continue;
+            
+            buttonUpgrade.InitUI(allData.ToList()[randomIndex[indexOfRandom]]);
+            indexOfRandom++;
+        }
     }
 
     public override void OnClose()
